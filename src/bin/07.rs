da4c0fce5cd1a679528805cce61817bc8a45bed9
Tobias Gaisberger <gaisberger.tobias@gmail.com>
2024-12-07 23:46:22 +0100

    add solution for day 7 part 2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -119,17 +119,83 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    #[derive(Clone, Hash, Eq, PartialEq, Debug)]
+    enum Operation2 {
+        Multiply,
+        Add,
+        Concatenate
+    }
+
+    fn perform_operation2(operation: Operation2, left_operand: u64, right_operand: u64) -> u64 {
+        match operation {
+            Operation2::Multiply => { left_operand * right_operand }
+            Operation2::Add => { left_operand + right_operand }
+            Operation2::Concatenate => { format!("{}{}", left_operand.to_string(), right_operand.to_string()).parse::<u64>().unwrap() }
+        }
+    }
+
+
+    fn create_permutations2(len: usize) -> Vec<Vec<Operation2>> {
+        if len == 0 {
+            return vec![vec![]];
+        }
+
+        let mut result = vec![];
+        for perm in create_permutations2(len - 1) {
+            let mut multiply_perm = perm.clone();
+            multiply_perm.push(Operation2::Multiply);
+            result.push(multiply_perm);
+
+            let mut add_perm = perm.clone();
+            add_perm.push(Operation2::Add);
+            result.push(add_perm);
+            
+            let mut concat_perm = perm.clone();
+            concat_perm.push(Operation2::Concatenate);
+            result.push(concat_perm);
+        }
+        result
+    }
+
+    fn check_operation2(exp_result: u64, operands: &Vec<u64>) -> bool {
+        let mut operands = operands.clone();
+        operands.reverse();
+
+        let permutations = create_permutations2(operands.len() - 1);
+        for operations in permutations {
+            let mut current_operands = operands.clone();
+
+            let mut res = current_operands.pop().unwrap();
+
+            for operation in operations {
+                let next_operand = current_operands.pop().unwrap();
+                res = perform_operation2(operation, res, next_operand);
+            }
+
+            if exp_result == res {
+                return true;
+            }
+        }
+        false
+    }
+    
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let operations = read_operations(reader)?;
+        let count = operations.iter()
+            .filter(|(result, operands)| { check_operation2(*result, operands) })
+            .map(|(result, _operands)| { result })
+            .sum::<u64>();
+
+        Ok(count as usize)
+    }
+    
+    assert_eq!(11387, part2(BufReader::new(TEST.as_bytes()))?);
+    
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
